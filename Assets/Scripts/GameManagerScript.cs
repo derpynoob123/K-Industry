a01b4adb38d557eca596609cd28197ecf98cf1d8
Thanatoshade <NoxCorvus629@gmail.com>
2023-12-05 10:54:43 +0800

    Project creation + Game Manager
    Added game manager script

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        bruh
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
