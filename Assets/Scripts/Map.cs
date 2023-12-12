287b2fa879643d008f7073dac92f05a4d55ab071
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-12 12:06:51 +0800

    Grids (#6)

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tilePrefab;
+    [SerializeField]
+    private Grid grid;
+
+    private void Start()
+    {
+        var worldPosition = grid.GetCellCenterWorld(new Vector3Int(1, 0, 0));
+        Instantiate(tilePrefab, worldPosition, Quaternion.identity);
+    }
+}
