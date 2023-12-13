7ae595d0b8d35022aa45c4c3c9a8d14f47df107c
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-13 12:40:13 +0800

    Updated map
    - removed start

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,37 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using UnityEngine;
 
-public class Map : MonoBehaviour
+public class Map
 {
-    [SerializeField]
-    private GameObject tilePrefab;
-    [SerializeField]
-    private Grid grid;
-
     public Dictionary<Vector2Int, Tile> Tiles { get; set; }
 
     private readonly List<GameObject> tiles = new();
     private const int rowLength = 8;
     private const int columnLength = 8;
 
-    private void Start()
-    {
-        Tiles = new();
-        InitialiseGrid();
-    }
-
-    private void InitialiseGrid()
+    public void InitialiseGrid()
     {
         for (int row = 0; row < rowLength; row++)
         {
             for (int column = 0; column < columnLength; column++)
             {
-                var position = new Vector3Int(row, column);
-                var worldPosition = grid.GetCellCenterWorld(position);
-                var tileGO = Instantiate(tilePrefab, worldPosition, Quaternion.identity);
-                tiles.Add(tileGO);
                 var gridPosition = new Vector2Int(row, column);
                 var tile = new Tile(gridPosition);
                 Tiles.Add(gridPosition ,tile);
