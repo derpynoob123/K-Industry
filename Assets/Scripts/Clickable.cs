13c1a69770f3428097f843dae0742c2d33dd5ce1
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-13 10:40:22 +0800

    Updated clickable

--- Assets/Scripts/Clickable.cs
+++ Assets/Scripts/Clickable.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Clickable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public UnityEvent Clicked;
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        Clicked.Invoke();
     }
 }
