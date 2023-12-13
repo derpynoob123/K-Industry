1c99be337c1326ba120e6f2dfa016d5bb1c0d054
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-13 10:32:09 +0800

    Updated building tile
    - Added facility variable

--- Assets/Scripts/BuildingTile.cs
+++ Assets/Scripts/BuildingTile.cs
@@ -6,6 +6,7 @@ public class BuildingTile
 {
     public bool Occupied { get; set; }
     public Vector2Int Position { get; }
+    public IFacility Facility { get; set; }
 
     public BuildingTile(Vector2Int position)
     {
