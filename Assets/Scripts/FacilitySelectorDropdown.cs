d436f6efcbfcbdf5c302690ceae5f45e6e6b0a1f
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-14 12:58:27 +0800

    Create facility selector dropdown

---
+++ Assets/Scripts/FacilitySelectorDropdown.cs
@@ -0,0 +1,4 @@
+public class FacilitySelectorDropdown : SelectorDropdown<IFacility>
+{
+
+}
--- Assets/Scripts/SelectorDropdown.cs
+++ Assets/Scripts/SelectorDropdown.cs
@@ -1,12 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public abstract class SelectorDropdown : MonoBehaviour
+public abstract class SelectorDropdown<T> : MonoBehaviour
 {
     [SerializeField]
     protected TMP_Dropdown dropdown;
     [SerializeField]
     protected string noSelectionMessage;
+
+    protected event Action<T> OptionSelected;
+    protected Dictionary<int, T> optionMenu = new();
+
+    virtual protected void Awake()
+    {
+        dropdown.options.Clear();
+    }
 }
