227cf47009d47e5694d35a55a65cb38eb1347fb3
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-14 13:04:51 +0800

    Updated selector dropdown
    - Added initialise options method

--- Assets/Scripts/SelectorDropdown.cs
+++ Assets/Scripts/SelectorDropdown.cs
@@ -14,8 +14,20 @@ public abstract class SelectorDropdown<T> : MonoBehaviour
     protected event Action<T> OptionSelected;
     protected Dictionary<int, T> optionMenu = new();
 
-    virtual protected void Awake()
+    private const int noSelectionOption = 0;
+
+    protected void Awake()
     {
         dropdown.options.Clear();
+    }
+
+    protected void InitialiseOptions(T[] options)
+    {
+        var option = new TMP_Dropdown.OptionData(noSelectionMessage);
+        dropdown.options.Add(option);
+        for (int optionIndex = 0; optionIndex < options.Length; optionIndex++)
+        {
+
+        }
     }
 }
