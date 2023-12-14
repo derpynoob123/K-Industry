e363ae06d491ad7cdf951eae66a042cbcf08ee48
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-14 14:30:22 +0800

    Updated selector dropdown
    - Changed initial options to virtual

--- Assets/Scripts/FacilitySelectorDropdown.cs
+++ Assets/Scripts/FacilitySelectorDropdown.cs
@@ -1,4 +1,16 @@
+using UnityEngine;
+
 public class FacilitySelectorDropdown : SelectorDropdown<IFacility>
 {
 
+
+    protected override void Awake()
+    {
+        dropdown.ClearOptions();
+    }
+
+    protected override void InitialiseOptions(IFacility[] options)
+    {
+        throw new System.NotImplementedException();
+    }
 }
--- Assets/Scripts/SelectorDropdown.cs
+++ Assets/Scripts/SelectorDropdown.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public abstract class SelectorDropdown<T> : MonoBehaviour
 {
@@ -13,21 +14,35 @@ public abstract class SelectorDropdown<T> : MonoBehaviour
 
     protected event Action<T> OptionSelected;
     protected Dictionary<int, T> optionMenu = new();
+    protected List<T> options;
 
     private const int noSelectionOption = 0;
 
-    protected void Awake()
+    virtual protected void Awake()
     {
-        dropdown.options.Clear();
+        dropdown.ClearOptions();
+        InitialiseOptions();
     }
 
-    protected void InitialiseOptions(T[] options)
+    virtual protected void InitialiseOptions()
     {
         var option = new TMP_Dropdown.OptionData(noSelectionMessage);
         dropdown.options.Add(option);
-        for (int optionIndex = 0; optionIndex < options.Length; optionIndex++)
+        for (int optionIndex = 0; optionIndex < options.Count; optionIndex++)
         {
-
+            string optionText = $"Option {optionIndex + 1}";
+            option = new TMP_Dropdown.OptionData(optionText);
+            dropdown.options.Add(option);
+            optionMenu.Add(optionIndex, options[optionIndex]);
         }
+    }
+    public void SelectOption()
+    {
+        if (dropdown.value == noSelectionOption)
+        {
+            return;
+        }
+        T selectedOption = optionMenu[dropdown.value];
+        OptionSelected.Invoke(selectedOption);
     }
 }
