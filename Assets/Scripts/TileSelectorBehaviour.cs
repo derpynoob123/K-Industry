22b66ea7fdc3f98e4b5981c2ef7cbd7ba1d8135a
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-18 12:34:20 +0800

    Imported reticle sprite

--- Assets/Scripts/TileSelectorBehaviour.cs
+++ Assets/Scripts/TileSelectorBehaviour.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class TileSelectorBehaviour : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject highlightIcon;
+
     private readonly TileSelector tileSelector = new();
 
     public void SelectTile(Tile tile)
     {
         tileSelector.SelectedTile = tile;
+    }
+
+    private void HighlightTile()
+    {
+        
     }
 }
