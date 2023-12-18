2db576d236189a2ac8d2a9270dfd6c38e04fef07
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-18 13:35:04 +0800

    Updated tile selector behaviour
    - Added highlight tile method

--- Assets/Scripts/TileSelectorBehaviour.cs
+++ Assets/Scripts/TileSelectorBehaviour.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class TileSelectorBehaviour : MonoBehaviour
 {
     [SerializeField]
-    private GameObject SelectedTileIcon;
+    private Transform selectedTileIcon;
 
     private readonly TileSelector tileSelector = new();
 
     public void SelectTile(Tile tile)
     {
         tileSelector.SelectedTile = tile;
+        HighlightTile(tile);
     }
 
-    private void HighlightTile()
+    private void HighlightTile(Tile tile)
     {
-        
+        selectedTileIcon.position = tile.TileTransform.position;
     }
 }
