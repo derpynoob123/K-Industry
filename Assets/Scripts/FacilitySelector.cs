cc5616f16c4e3b51cc2117e30bfc10afb407386d
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-20 12:26:40 +0800

    Created facility selector behaviour

--- Assets/Scripts/FacilitySelector.cs
+++ Assets/Scripts/FacilitySelector.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FacilitySelector : MonoBehaviour
+public class FacilitySelector
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public IFacility SelectedFacility { get; set; }
 }
