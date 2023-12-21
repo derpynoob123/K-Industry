265807d4b64d0593c97061c02ad362a66a1be5c1
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-21 13:40:13 +0800

    Updated tile selection highlight
    - Added reference to tile selector

--- Assets/Scripts/TileSelectionHighlight.cs
+++ Assets/Scripts/TileSelectionHighlight.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class TileSelectionHighlight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private TileSelectorBehaviour tileSelector;
 }
