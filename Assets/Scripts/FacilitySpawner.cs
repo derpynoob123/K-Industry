9e4f49cd78d457276ebea1a723e77968ce31f389
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-22 15:17:19 +0800

    Updated facility spawner
    - Added facility variable
    - Added pool variable
    - Added spawn count variable

--- Assets/Scripts/FacilitySpawner.cs
+++ Assets/Scripts/FacilitySpawner.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class FacilitySpawner
 {
+    private readonly IFacility facility;
+    private readonly ObjectPool<IFacility> pool;
+    private readonly int spawnCount;
 
+    public FacilitySpawner(IFacility facility, int spawnCount)
+    {
+        this.facility = facility;
+        this.spawnCount = spawnCount;
+    }
 }
