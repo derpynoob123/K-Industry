0be88eb360aa4463847a3e33da77f0db2c5e1edf
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2023-12-28 14:38:26 +0800

    Created ware

--- Assets/Scripts/PackingFacility.cs
+++ Assets/Scripts/PackingFacility.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PackingFacility : Facility
+public class PackingFacility : Facility, IWare
 {
     public PackingFacility()
     {
         ID = FacilityID.PACKING;
     }
+
+    public int CurrentCapacity { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public int TotalCapacity { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 }
