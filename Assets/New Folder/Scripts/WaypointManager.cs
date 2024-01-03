59f5bb544e44484137f5bb5626fd26e7910b7c0f
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2024-01-03 11:15:44 +0800

    Updated waypoint manager
    - Added initialise graph method

--- Assets/New Folder/Scripts/WaypointManager.cs
+++ Assets/New Folder/Scripts/WaypointManager.cs
@@ -7,8 +7,28 @@ public class WaypointManager : MonoBehaviour
     [SerializeField]
     private GameObject[] wayPoints;
     [SerializeField]
-    private Path[] connections;
+    private Path[] paths;
 
     private PathfindingGraph graph = new();
     private AStarPathFinder pathFinder = new();
+
+    private void Awake()
+    {
+        InitialiseGraph();
+    }
+
+    private void InitialiseGraph()
+    {
+        for (int waypointIndex = 0; waypointIndex < wayPoints.Length; waypointIndex++)
+        {
+            GameObject waypoint = wayPoints[waypointIndex];
+            graph.AddNode(waypoint);
+        }
+        for (int pathIndex = 0; pathIndex < paths.Length; pathIndex++)
+        {
+            Path path = paths[pathIndex];
+            graph.AddConnection(path.NodeA, path.NodeB);
+            graph.AddConnection(path.NodeB, path.NodeA);
+        }
+    }
 }
