6047a082cc821e509004ae63c16b3a5040a7c181
Thanatoshade <NoxCorvus629@gmail.com>
2024-01-04 15:45:33 +0800

    Time counter in progrss

--- Assets/New Folder/Scripts/GameManagerScript.cs
+++ Assets/New Folder/Scripts/GameManagerScript.cs
@@ -13,8 +13,12 @@ public class GameManagerScript : MonoBehaviour
     public float money;
     public float moneyText;
 
-    public float time;
+    //Time variables
+    public int minMul = 60;
+    public float minutesTime;
+    public int hoursTime;
     public int day;
+    public int dayCount;
     public int week;
 
     //TextUI
@@ -31,14 +35,54 @@ public class GameManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        minutesTime = 0;
+        hoursTime = 0;
+        day = 0;
+        dayCount = 0;
+        week = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        #region In-game time counting
+        //Timer counting
+        minutesTime += minMul * Time.deltaTime;
+        int minInt = (int)minutesTime;
+        Debug.Log(minutesTime);
 
+        if (minutesTime >= 60)
+        {
+            minutesTime = 0;
+            hoursTime++;
+        }
+
+        if (hoursTime >= 24)
+        {
+            hoursTime = 0;
+            day++;
+            dayCount++;
+        }
+
+        if (dayCount >= 7)
+        {
+            week++;
+            dayCount = 0;
+        }
+        #endregion
+
+        #region Slow down In-game time for player change
+        if (hoursTime >= 17)
+        {
+            minMul = 1;
+        }
+        #endregion
+
+        #region Display variables onto UI
+        timeText.text = $"{hoursTime}:{minInt}";
+        dayText.text = $"Day: {day}";
+        weekText.text = $"Week: {week}";
+        #endregion
     }
 
     //Start screen to main play screen
