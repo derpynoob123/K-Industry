fd230c5e15f596ad133d0b3f2dcea1588089ba58
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2024-01-09 15:08:22 +0800

    Updated facility spawner
    - Removed build method

--- Assets/New Folder/Scripts/FacilitySpawnerBehaviour.cs
+++ Assets/New Folder/Scripts/FacilitySpawnerBehaviour.cs
@@ -24,8 +24,6 @@ public class FacilitySpawnerBehaviour : MonoBehaviour
     public event Action<Tile, GameObject> FacilitySpawned;
 
     private Dictionary<FacilityID, GameObject> prefabIDs;
-    //list of active facilities
-    private Dictionary<Tile, GameObject> spawnedFacilities = new();
 
     private void Awake()
     {
@@ -42,23 +40,12 @@ public class FacilitySpawnerBehaviour : MonoBehaviour
         }
     }
 
-    public void Build()
+    public void Spawn()
     {
         Tile tile = tileSelector.GetSelectedTile();
-        if (spawnedFacilities.ContainsKey(tile))
-        {
-            Debug.Log("Cannot build on tile containing an existing facility!");
-            return;
-        }
-        Spawn(tile);
-    }
-
-    public void Spawn(Tile tile)
-    {
         FacilityID id = facilitySelector.GetSelectedFacility();
         GameObject prefab = prefabIDs[id];
         GameObject newFacility = Instantiate(prefab);
-        spawnedFacilities.Add(tile, newFacility);
         newFacility.transform.position = tile.TileTransform.position;
         newFacility.transform.SetParent(facilityParent);
         FacilitySpawned.Invoke(tile, newFacility);
