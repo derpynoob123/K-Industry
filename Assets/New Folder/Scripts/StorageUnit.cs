e832e94f2fbab26f977b1a4201d63ae07f68738d
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2024-01-10 14:58:56 +0800

    Updated storage unit
    - Added maximum capacity variable
    - Added current capacity variable

--- Assets/New Folder/Scripts/StorageUnit.cs
+++ Assets/New Folder/Scripts/StorageUnit.cs
@@ -2,17 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StorageUnit : MonoBehaviour
+public class StorageUnit
 {
-    // Start is called before the first frame update
-    void Start()
+    private int maximumCapacity;
+    public int MaximumCapacity
     {
-        
+        get
+        {
+            return maximumCapacity;
+        }
+        private set
+        {
+            if (value <= 0)
+            {
+                maximumCapacity = 1;
+            }
+            else
+            {
+                maximumCapacity = value;
+            }
+        }
+    }
+    private int currentCapacity;
+    public int CurrentCapacity
+    {
+        get
+        {
+            return currentCapacity;
+        }
+
+        set
+        {
+            if (value < 0)
+            {
+                currentCapacity = 0;
+            }
+            else if (value > MaximumCapacity)
+            {
+                currentCapacity = MaximumCapacity;
+            }
+            else
+            {
+                currentCapacity = value;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public StorageUnit(int maximumCapacity)
     {
-        
+        MaximumCapacity = maximumCapacity;
     }
 }
