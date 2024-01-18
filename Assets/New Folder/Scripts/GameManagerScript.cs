0c908e661d93ed280e0e32af1cf44799c1825425
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2024-01-18 11:29:49 +0800

    Skip button(revert ingame time to 1hr/s) (#28)
    Co-authored-by: Thanatoshade <NoxCorvus629@gmail.com>

--- Assets/New Folder/Scripts/GameManagerScript.cs
+++ Assets/New Folder/Scripts/GameManagerScript.cs
@@ -16,6 +16,7 @@ public class GameManagerScript : MonoBehaviour
     public float time;
     public int day;
     public int week;
+    public bool skipCheck;
 
     //TextUI
     public TextMeshProUGUI timeText;
@@ -31,19 +32,65 @@ public class GameManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        minutesTime = 0;
+        hoursTime = 0;
+        day = 0;
+        dayCount = 0;
+        week = 0;
+
+        skipCheck = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (minutesTime >= 60)
+        {
+            minutesTime = 0;
+            hoursTime++;
+        }
 
+        if (hoursTime >= 24)
+        {
+            hoursTime = 0;
+            day++;
+            dayCount++;
+            skipCheck = false;
+        }
+
+        if (dayCount >= 7)
+        {
+            week++;
+            dayCount = 0;
+        }
+        #endregion
+
+        #region Slow down In-game time for player change
+        if ((hoursTime >= 17) && (skipCheck == false))
+        {
+            minMul = 1;
+        }
+        else
+        {
+            minMul = 60;
+        }
+        #endregion
+
+        #region Display variables onto UI
+        timeText.text = $"{hoursTime}:{minInt}";
+        dayText.text = $"Day: {day}";
+        weekText.text = $"Week: {week}";
+        #endregion
     }
 
     //Start screen to main play screen
     public void StartGame()
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void SkipButton()
+    {
+        skipCheck = true;
     }
 }
