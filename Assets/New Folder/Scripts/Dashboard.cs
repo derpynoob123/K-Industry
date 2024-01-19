a0e85629614444eb9ed4a220e6cecfbbc6f08d95
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2024-01-19 11:08:40 +0800

    Created vehicle text

--- Assets/New Folder/Scripts/Dashboard.cs
+++ Assets/New Folder/Scripts/Dashboard.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Dashboard : MonoBehaviour
 {
+    [SerializeField]
+    private VehicleFleet vehicleFleet;
 
+    [SerializeField]
+    private TMP_Text vehicleCountText;
 
     private void LateUpdate()
     {
         
+    }
+
+    private void UpdatedDashboard()
+    {
+
     }
 }
