7c9a0cb2707c54fb6443a231cb839422f94c85f0
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2024-01-19 11:38:15 +0800

    Created current money text

--- Assets/New Folder/Scripts/Dashboard.cs
+++ Assets/New Folder/Scripts/Dashboard.cs
@@ -10,6 +10,8 @@ public class Dashboard : MonoBehaviour
 
     [SerializeField]
     private TMP_Text vehicleCountText;
+    [SerializeField]
+    private TMP_Text currentMoneyText;
 
     private void LateUpdate()
     {
@@ -20,5 +22,8 @@ public class Dashboard : MonoBehaviour
     {
         string vehicleCount = vehicleFleet.GetVehicleCount().ToString();
         vehicleCountText.text = $"Vehicle: {vehicleCount}";
+
+        string currentMoney = PlayerResource.Money.ToString();
+        currentMoneyText.text = $"Current Money: {currentMoney}";
     }
 }
