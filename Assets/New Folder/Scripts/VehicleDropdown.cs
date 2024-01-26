db6f71a9de896e7cf3ade97beb1e2ed62eca73ce
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2024-01-26 14:22:43 +0800

    Created vehicle dropdown

---
+++ Assets/New Folder/Scripts/VehicleDropdown.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleDropdown : SelectionDropdown<VehicleController>
+{
+    protected override void AddDeselectedObservers()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void AddDropdownOptions()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void AddOptionSelectedObservers()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void SetOptions()
+    {
+        throw new System.NotImplementedException();
+    }
+}
