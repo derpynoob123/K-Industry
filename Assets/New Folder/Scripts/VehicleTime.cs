bdea90a61889a19df0812ba9ddec064926e0ea7a
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2024-01-29 16:24:59 +0800

    Updated vehicle time
    - Added hours of day, minutes of day properties

--- Assets/New Folder/Scripts/VehicleTime.cs
+++ Assets/New Folder/Scripts/VehicleTime.cs
@@ -10,35 +10,73 @@ public class VehicleTime : MonoBehaviour
 
     [SerializeField]
     private int[] hoursOfDay = { 0, 0 };
+    public int[] HoursOfDay
+    {
+        get => hoursOfDay;
+        private set
+        {
+            if (value[0] > hourLimit)
+            {
+                Debug.LogError("Hour exceeds the limit!");
+                return;
+            }
+            if (value[0] < 0 || value[1] < 0)
+            {
+                Debug.LogError("Hour less than zero!");
+                return;
+            }
+            hoursOfDay = value;
+        }
+    }
 
     [SerializeField]
     private int[] minutesOfDay = { 0, 0 };
+    public int[] MinutesOfDay
+    {
+        get => minutesOfDay;
+        private set
+        {
+            if (value[0] > minuteLimit)
+            {
+                Debug.LogError("Minute exceeds the limit!");
+                return;
+            }
+            if (value[0] < 0 || value[1] < 0)
+            {
+                Debug.LogError("Minute less than zero!");
+                return;
+            }
+            minutesOfDay = value;
+        }
+    }
 
     private const string o = "0";
+    private const int hourLimit = 2;
+    private const int minuteLimit = 6;
 
     private void Update()
     {
         string hours = time.hoursTime.ToString();
-        if (hours.Length != hoursOfDay.Length)
+        if (hours.Length != HoursOfDay.Length)
         {
             hours = $"{o}{hours}";
         }
-        for (int hourIndex = 0; hourIndex < hoursOfDay.Length; hourIndex++)
+        for (int hourIndex = 0; hourIndex < HoursOfDay.Length; hourIndex++)
         {
             string hour = hours.Substring(hourIndex, 1);
-            hoursOfDay[hourIndex] = int.Parse(hour);
+            HoursOfDay[hourIndex] = int.Parse(hour);
         }
 
         int minutesInInt = Mathf.FloorToInt(time.minutesTime);
         string minutes = minutesInInt.ToString();
-        if (minutes.Length != minutesOfDay.Length)
+        if (minutes.Length != MinutesOfDay.Length)
         {
             minutes = $"{o}{minutes}";
         }
-        for (int minuteIndex = 0; minuteIndex < minutesOfDay.Length; minuteIndex++)
+        for (int minuteIndex = 0; minuteIndex < MinutesOfDay.Length; minuteIndex++)
         {
             string minute = minutes.Substring(minuteIndex, 1);
-            minutesOfDay[minuteIndex] = int.Parse(minute);
+            MinutesOfDay[minuteIndex] = int.Parse(minute);
         }
     }
 }
