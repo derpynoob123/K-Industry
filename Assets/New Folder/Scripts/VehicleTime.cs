60cc014823c212e8033c6f48abdfcdeffdc34ec5
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2024-01-29 15:55:58 +0800

    Updated vehicle time
    - Added minutes of day variable

--- Assets/New Folder/Scripts/VehicleTime.cs
+++ Assets/New Folder/Scripts/VehicleTime.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class VehicleTime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameManagerScript time;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private int[] minutesOfDay = { 0, 0 };
+
+    private const string o = "0";
+
+    private void Update()
     {
-        
+        int minutesInInt = Mathf.FloorToInt(time.minutesTime);
+        string minutes = minutesInInt.ToString();
+        if (minutes.Length != minutesOfDay.Length)
+        {
+            minutes = $"{o}{minutes}";
+        }
+        for (int minuteIndex = 0; minuteIndex < minutesOfDay.Length; minuteIndex++)
+        {
+            string minute = minutes.Substring(minuteIndex, 1);
+            minutesOfDay[minuteIndex] = int.Parse(minute);
+        }
     }
 }
