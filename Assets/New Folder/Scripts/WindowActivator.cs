dd023ab92a4790821ab79285756ae118559b3c27
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2024-01-29 14:34:24 +0800

    Updated window activator
    - Created window class

--- Assets/New Folder/Scripts/WindowActivator.cs
+++ Assets/New Folder/Scripts/WindowActivator.cs
@@ -1,24 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class WindowActivator : MonoBehaviour
 {
+    [Serializable]
+    public class Window
+    {
+        public ToggleButton Toggle;
+        public GameObject Screen;
+    }
+
+    [SerializeField]
+    private Window[] windows;
     [SerializeField]
     private WindowSelector windowSelector;
     [SerializeField]
     private GameObject activeWindow;
 
+    private readonly Dictionary<GameObject, ToggleButton> toggles = new();
+
     private void Awake()
     {
         windowSelector.AddObserverToSelectedEvent(ShowWindow);
         windowSelector.AddObserverToDeselectedEvent(HideWindow);
+
+        InitialiseToggles();
+    }
+
+    private void InitialiseToggles()
+    {
+        for (int windowIndex = 0; windowIndex < windows.Length; windowIndex++)
+        {
+            Window window = windows[windowIndex];
+            toggles.Add(window.Screen, window.Toggle);
+        }
     }
 
     private void ShowWindow()
     {
         if (activeWindow)
         {
+            ToggleButton toggle = toggles[activeWindow];
+            toggle.Untoggle();
             HideWindow();
         }
 
