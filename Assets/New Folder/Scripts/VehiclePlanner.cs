d2936a979f8ec3f29f253d847dd4c67c96e88ec3
derpynoob123 <104753675+derpynoob123@users.noreply.github.com>
2024-01-31 11:54:23 +0800

    Updated vehicle planner
    - Added update plan method

--- Assets/New Folder/Scripts/VehiclePlanner.cs
+++ Assets/New Folder/Scripts/VehiclePlanner.cs
@@ -1,14 +1,32 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
 public class VehiclePlan
 {
+    public ClockHour StartHour;
+    public ClockMinute StartMinute;
     public List<VehicleTask> Tasks = new();
 }
 
 public class VehiclePlanner : MonoBehaviour
 {
+    [SerializeField]
+    private DayClock clock;
+
     public List<VehiclePlan> Plans = new();
+
+    private VehiclePlan currentPlan;
+
+    private void Awake()
+    {
+        clock.MinutePassed += UpdatePlan;
+    }
+
+    private void UpdatePlan()
+    {
+
+    }
 }
